6e87e569ab0a15c2d0c0980451be0ab32c75506e
po1onius <po1onius@outlook.com>
2025-07-26 18:18:46 +0800

    service test

---
+++ ch0r0ng/services/network.scm
@@ -0,0 +1,46 @@
+(define-module (ch0r0ng services network)
+#:use-module (rosenthal packages binaries)
+#:export (home-clash-service-type
+          home-clash-configuration))
+
+
+(define-configuration home-clash-configuration
+  (clash
+   (file-like mihomo-bin)
+   "The clash package.")
+
+  (config-path
+   (string ".config/clash")
+   "config path")
+  (no-serialization))
+
+(define clash-shepherd-service
+  (match-record-lambda
+   <clash-configuration>
+   (clash config-path)
+   (list
+    (shepherd-service
+     (documentation "Run clash.")
+     (provision (list 'clash))
+     (start
+      #~(make-forkexec-constructor
+	 (list
+	  (let ((mihomo-cmd
+		 #$(file-append clash "/bin/mihomo"))
+		(clash-cmd
+		 #$(file-append clash "/bin/clash")))
+	    (if (file-exists? mihomo-cmd)
+		mihomo-cmd
+		clash-cmd))
+	  "-d" #$config-path)))
+     (stop #~(make-kill-destructor))))))
+
+(define home-clash-service-type
+  (service-type
+   (name 'clash)
+   (extensions
+    (list (service-extension
+	   home-shepherd-service-type
+	   clash-shepherd-service)))
+   (description "clash service")
+   (default-value (home-clash-configuration))))
