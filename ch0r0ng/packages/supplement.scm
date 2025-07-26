fb483055dd856056ced7386a34bbe3b7106946f1
po1onius <po1onius@outlook.com>
2025-07-26 17:47:52 +0800

    gcc change to gcc-toolchain

--- ch0r0ng/packages/supplement.scm
+++ ch0r0ng/packages/supplement.scm
@@ -2,7 +2,7 @@
 (define-module (ch0r0ng packages supplement)
   #:use-module (guix packages)
   #:use-module (guix build-system trivial)
-  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages commencement)
   #:use-module (guix packages))
 
 
@@ -12,7 +12,7 @@
     (version "0.1")
     (source #f) ; 无源码，仅生成符号链接
     (build-system trivial-build-system)
-    (propagated-inputs `(("gcc" ,gcc))) ; 依赖 GCC 包
+    (propagated-inputs `(("gcc-toolchain" ,gcc-toolchain))) ; 依赖 GCC 包
     (arguments
      `(#:modules ((guix build utils))
        #:builder
@@ -21,7 +21,7 @@
          ;; 创建目标目录结构
          (mkdir-p (string-append %output "/bin"))
          ;; 创建符号链接：cc -> gcc
-         (symlink (string-append (assoc-ref %build-inputs "gcc") "/bin/gcc")
+         (symlink (string-append (assoc-ref %build-inputs "gcc-toolchain") "/bin/gcc")
                   (string-append %output "/bin/cc")))))
     (synopsis "Symbolic link for GCC as 'cc'")
     (description "This package provides a symbolic link from 'cc' to 'gcc'.")
